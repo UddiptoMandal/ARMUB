f433f5aa77173ae3c6fc3d8b9291e76eea08030a
UddiptoMandal <uddiptomandal2006@gmail.com>
2025-07-08 22:22:39 +0530

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,112 @@
+import numpy as np
+import math
+# This code works for any Hadamard matrix which is a multiple of 4, and creates epsilon-Hadamard matrices of order 4n-t, t ={1,2,3}
+t = 1 
+ortho_threshold = 0.0000000000000001  #threshold for dot product of rows in an orthogonal matrix, not 0 exactly due to floating point round off error
+
+# Create the epsilon-Hadamard matrices Y1, Y2
+"""
+Y1, Y2 = epsilon-Hadamard matrices of order 4n-t
+U, V, W, D = sub-blocks of Hadamard matrices when written in block form
+I = identity matrix or order t
+inv1, inv2 = Inverse of (I+U) and (I-U) respectively
+"""
+def eHadamard(H, n, t):
+    # separating the blocks of the Hadamard matrix
+    U = H[0:t, 0:t]
+    V = H[0:t, t:4*n]
+    W = H[t:4*n, 0:t]
+    D = H[t:4*n, t:4*n]
+    I = np.eye(t) 
+    try:
+        inv1 = np.linalg.inv(I + U)
+        Y1 = (D - W @inv1@V)
+    except np.linalg.LinAlgError:
+        print("Error: (I + U) is singular and cannot be inverted. Try a different Hadamard matrix")
+        exit(0)
+    try:
+        inv2 = np.linalg.inv(I-U)
+        Y2 = (D + W@inv2@V)
+    except np.linalg.LinAlgError:
+        print("Error: (I - U) is singular and cannot be inverted. Try a different Hadamard matrix")
+        exit(0)
+    return Y1, Y2
+
+# Orthogonality check of 4n-t x 4n-t matrix Y
+"""
+flag = flag variable for orthogonality of matrix Y
+ortho_threshold = threshold below which the dot product of rows should be for the matrix to be orthogonal
+it is not 0 here, due to floating point rounding errors.
+"""
+def ortho(Y, n, t):
+    flag = 0
+    for i in range(4*n-t):
+        for j in range(4*n-t):
+            if i != j:
+                if (np.dot(Y[i], Y[j]) > ortho_threshold or np.dot(Y[i], Y[j]) < -1*ortho_threshold):
+                    flag = 1
+                    break
+    if flag == 0:
+        print("Yes, orthogonal matrix")
+    else:
+        print("No, not orthogonal matrix")
+
+# calculate epsilon of epsilon-Hadamard matrix Y
+"""
+e = epsilon value for each element in matrix
+max = maximum value of |e|
+"""
+def epsilon(Y, n, t):
+    max = 0
+    for i in range(4*n-t):
+        for j in range(4*n-t):
+            e = np.abs(Y[i][j])*math.sqrt(4*n-t)-1
+            if abs(e) > max:
+                max = abs(e)
+    return max
+
+# Read hadamard matrix from file 
+"""
+Read Hadamard matrix from filepath
+file contains + and - characters, + representing 1 and - representing -1 of an unnormalized Hadamard matrix
+database of Hadamard matrices with such files: http://neilsloane.com/hadamard/
+lines = list of strings from the file
+line = each string in lines
+char = each character in line
+d = size of Hadamard matrix = 4*n
+"""
+def read_hadamard(filepath):
+    with open(filepath, 'r') as file:
+        lines = file.readlines()
+
+    matrix = []
+    for line in lines:
+        row = []
+        for char in line.strip():
+            if char == '+':
+                row.append(1)
+            elif char == '-':
+                row.append(-1)
+        matrix.append(row)
+    H = np.array(matrix)
+    d = H.shape[0]
+    H = (1/np.sqrt(d))*H
+    print(f"Loaded Hadamard matrix of shape {H.shape}")
+    return H, (d//4)
+
+# Input file path here to use a different Hadamard matrix
+H, n = read_hadamard("had.160.tpal.txt")
+print("Hadamard matrix is: ", H)
+Y1, Y2= eHadamard(H, n, t)
+print("The epsilon-Hadamard matrices we get are: ")
+print("Y1: ", Y1)
+print("Y2: ", Y2)
+# Check for orthogonality of the epsilon-Hadamard matrices
+print("Y1 check for orthogonality:")
+ortho(Y1, n, t)
+print("Y2 check for orthogonality:")
+ortho(Y2, n, t)
+print("Epsilon values for the Hadamard matrix used, and the epsilon-Hadamard matrices created")
+print("e_Hadamard: ", epsilon(H, n, 0))
+print("e_Y1: ", epsilon(Y1, n, t))
+print("e_Y2: ", epsilon(Y2, n, t))
